112e7bad78c9575b7489bccdf079ea60418e4d0e
Hien_Nguyen <h.phuong211296@gmail.com>
2021-02-23 09:41:56 +0700

    1

---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        Point2D a = new Point2D(3,4);
+        System.out.println(a);
+        Point3D b = new Point3D(2,4,5);
+        System.out.println(b);
+    }
+}
